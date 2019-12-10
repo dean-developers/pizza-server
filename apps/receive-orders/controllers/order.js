e73c1132744e0aeab08ad03c691223575f40b34e
denissemo <denissemo1999@gmail.com>
2019-12-11 01:48:39 +0200

    add delete order

--- apps/receive-orders/controllers/order.js
+++ apps/receive-orders/controllers/order.js
@@ -1,7 +1,7 @@
 require('sugar')();
 
 import helpers from '../../../lib/helpers';
-
+import model from '../../../model';
 
 const calculate = async (ctx) => {
     const { pizzas } = ctx.request.body;
@@ -13,6 +13,33 @@ const calculate = async (ctx) => {
     ctx.body = await helpers.calculateOrder(pizzas);
 };
 
+const delete_ = async (ctx) => {
+    const { id: orderId } = ctx.params;
+
+    if (!orderId) {
+        ctx.throw(400, 'badRequest');
+    }
+
+    const pizzaOrder = await model.PizzaOrder.findAll({
+        where: {
+            orderId
+        }
+    });
+
+    if (!pizzaOrder.length) {
+        ctx.throw(404, 'orderNotFound');
+    }
+
+    await model.PizzaOrder.destroy({
+        where: {
+            id: { [model.Sequelize.Op.in]: pizzaOrder.map('id') }
+        }
+    });
+
+    ctx.body = pizzaOrder;
+};
+
 module.exports = {
-    calculate
+    calculate,
+    delete_
 };
